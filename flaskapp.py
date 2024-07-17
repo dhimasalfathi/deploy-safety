c055dd90b49b81cedd312f50e7ff4f82dc0b5797
dhimasalfathi <vitosmart2@gmail.com>
2024-07-17 14:02:32 +0700

    update flaskapi.py

--- flaskapp.py
+++ flaskapp.py
@@ -59,10 +59,11 @@ def generate_frames(path_x=""):
 
 
 def generate_frames_web(path_x):
-    yolo_output = video_detection(path_x)
+    # Create a temporary output path for the webcam stream
+    output_path = os.path.join(app.config["UPLOAD_FOLDER"], "webcam_output.mp4")
+    yolo_output = video_detection(path_x, output_path)
     for detection_ in yolo_output:
         ref, buffer = cv2.imencode(".jpg", detection_)
-
         frame = buffer.tobytes()
         yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
 
